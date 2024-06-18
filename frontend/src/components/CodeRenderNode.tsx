2def665489ba5a24faae30c30c23f50351819789
Yuwen Lu <ylu48@apple.com>
2024-06-18 12:02:40 -0700

    Switch resize style

--- frontend/src/components/CodeRenderNode.tsx
+++ frontend/src/components/CodeRenderNode.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { Handle, Position, NodeProps, NodeResizer } from 'reactflow';
+import { Handle, Position, NodeProps, NodeResizeControl } from 'reactflow';
 import { LuUndo2, LuCheck, LuTerminal } from 'react-icons/lu';
 import CodeRenderIframe from './CodeRenderIframe';
 
 const CodeRenderNode: React.FC<NodeProps> = ({ data, selected }) => {
 
     return (
-        <div className="flex flex-col items-center p-5 text-white bg-stone-900/70 rounded-lg border-2 border-stone-400 w-full h-full">
+        <div className="flex flex-col items-center px-20 py-5 text-white bg-stone-900/70 rounded-lg border-2 border-stone-400 w-full h-full">
             <div className='font-semibold text-xl mb-5'>
                 Source Code Render
             </div>
@@ -21,9 +21,35 @@ const CodeRenderNode: React.FC<NodeProps> = ({ data, selected }) => {
                     <span className='ml-2'>Show Code</span>
                 </button>
             </div>
-            <NodeResizer isVisible={selected} minHeight={900} minWidth={400}/>
+            <NodeResizeControl style={{ visibility: selected ? "visible" : "hidden" }}  minWidth={500} minHeight={900}>
+                <ResizeIcon />
+            </NodeResizeControl>
         </div>
     );
 };
+
+function ResizeIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="#fff"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        style={{ position: 'absolute', right: 5, bottom: 5 }}
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <polyline points="16 20 20 20 20 16" />
+        <line x1="14" y1="14" x2="20" y2="20" />
+        <polyline points="8 4 4 4 4 8" />
+        <line x1="4" y1="4" x2="10" y2="10" />
+      </svg>
+    );
+  }
+  
 
 export default CodeRenderNode;
