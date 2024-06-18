611040a8b853bd98e9ad9940c59a624336ceef3b
Yuwen Lu <ylu48@apple.com>
2024-06-18 11:15:37 -0700

    Enable reize. not sure why height does not work

--- frontend/src/components/CodeRenderIframe.tsx
+++ frontend/src/components/CodeRenderIframe.tsx
@@ -29,7 +29,7 @@ const CodeRenderIframe = () => {
     }, []);
 
     return (
-        <div className="iframe-container">
+        <div className="iframe-container w-full">
             <iframe ref={iframeRef} title="Tailwind iframe" style={{ width: '100%', height: '400px', border: 'none' }} />
         </div>
     );
--- frontend/src/components/CodeRenderNode.tsx
+++ frontend/src/components/CodeRenderNode.tsx
@@ -1,19 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { Handle, Position, NodeProps } from 'reactflow';
+import { Handle, Position, NodeProps, NodeResizer } from 'reactflow';
 import { LuTrash2, LuUndo2, LuCheck } from 'react-icons/lu';
 import CodeRenderIframe from './CodeRenderIframe';
 
 const CodeRenderNode: React.FC<NodeProps> = ({ data }) => {
 
     return (
-        <div className="image-display-node flex flex-col items-center p-5 text-white bg-stone-900/70 rounded-lg border-2 border-stone-400 relative">
+        <div className="code-render-node flex flex-col items-center p-5 text-white bg-stone-900/70 rounded-lg border-2 border-stone-400 relative">
             <div className='font-semibold text-xl mb-5'>
                 Source Code Render
             </div>
-            <div className='image-display-section relative'>
-                <CodeRenderIframe />
-            </div>
+            <CodeRenderIframe />
             <div className='flex flex-row'>
                 <button
                     className={"flex items-center rounded-full mt-6 mx-2 px-5 py-3 text-white font-semibold focus:outline-none bg-zinc-700 hover:bg-zinc-900"}
@@ -22,24 +20,8 @@ const CodeRenderNode: React.FC<NodeProps> = ({ data }) => {
                     <LuTrash2 />
                     <span className='ml-2'>Clear</span>
                 </button>
-                <button
-                    className={"flex items-center rounded-full mt-6 mx-2 px-5 py-3 text-white font-semibold focus:outline-none bg-zinc-700 hover:bg-zinc-900"}
-                // onClick={undoCanvas}
-                >
-                    <LuUndo2 />
-                    <span className='ml-2'>Undo</span>
-                </button>
-                <button
-                    className={"flex items-center rounded-full mt-6 mx-2 px-5 py-3 text-white font-semibold focus:outline-none bg-zinc-700 hover:bg-zinc-900"}
-                // onClick={() => dissectImage(data.image)}
-                // onClick={() => {
-                //     getMergedSubImages();
-                // }}
-                >
-                    <LuCheck />
-                    <span className='ml-2'>Done</span>
-                </button>
             </div>
+            <NodeResizer />
         </div>
     );
 };
