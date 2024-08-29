e510aca84786ad28a44143c6f82aecfc4a139949
Yuwen Lu <ylu48@apple.com>
2024-08-29 13:41:11 -0700

    add alert for refresh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ReactFlowProvider } from 'reactflow';
 import FlowComponent from './components/FlowComponent';
 
 
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) : string | void => {
+      // Standard message (some browsers override this)
+      const message = "Are you sure you want to leave? Changes you made may not be saved.";
+      event.returnValue = message; // For most browsers
+      return message; // For some browsers
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
 
   return (
     <ReactFlowProvider>
