fdb35d4e28648d7548f06774f99e5ddca7209cde
Yuwen <ylu23@nd.edu>
2025-08-06 00:38:42 -0400

    fix next and react weird bug

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  experimental: {
-    esmExternals: false,
-  },
  
   transpilePackages: ['lucide-react'],
   webpack: (config) => {
